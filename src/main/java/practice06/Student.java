cf2d0c8be2594b652338b2166bd33df48a7977f1
ooxiaoyan <810978527@qq.com>
2018-04-08 01:39:18 +0800

    commit practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,29 @@
 package practice06;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce().concat(" I am a Student. I am at Class " + getKlass() + ".");
+    }
 }
