2f2d77a0b9af356746458af4e2f8fd21ebb07dec
ooxiaoyan <810978527@qq.com>
2018-04-08 02:02:25 +0800

    commit practice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,26 @@
 package practice07;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+
+    Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce().concat(" I am a Student. I am at Class " + getKlass().getNumber() + ".");
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+import practice06.Person;
+
+public class Teacher extends Person {
+    Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (getKlass() != null) {
+            return super.introduce().concat(" I am a Teacher. I teach Class " + getKlass().getNumber() + ".");
+        } else {
+            return super.introduce().concat(" I am a Teacher. I teach No Class.");
+        }
+    }
+
+    public String introduceWith(Student jerry) {
+        if (jerry.getKlass().getNumber() == getKlass().getNumber()) {
+            return super.introduce().concat(" I am a Teacher. I teach " + jerry.getName() + ".");
+        } else {
+            return super.introduce().concat(" I am a Teacher. I don't teach " + jerry.getName() + ".");
+        }
+    }
 }
