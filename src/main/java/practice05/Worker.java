0fd2ec0adb61be8b5f5bc7ef19285a94a3a9fb86
ooxiaoyan <810978527@qq.com>
2018-04-08 01:30:51 +0800

    commit practice05

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,14 @@
 package practice05;
 
-public class Worker {
+import practice03.Person;
+
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce().concat(" I am a Worker. I have a job.");
+    }
 }
