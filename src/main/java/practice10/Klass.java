10d635f6f1837e7d2a2f57db4affe9ae477c97d2
ooxiaoyan <810978527@qq.com>
2018-04-08 04:36:14 +0800

    commit practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,57 @@
 package practice10;
 
-public class Klass {
+public class Klass implements Comparable{
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Klass(int number, Student leader) {
+        this.number = number;
+        this.leader = leader;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + getNumber();
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().getNumber() != getNumber()) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.getKlass().setNumber(getNumber());
+    }
+
+    @Override
+    public int compareTo(Object obj) {
+        if (!(obj instanceof Klass))
+            throw new RuntimeException("no Klass");
+
+        Klass klass = (Klass) obj;
+        int num = new Integer(this.number).compareTo(new Integer(klass.number));
+        return num;
+    }
 }
--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,24 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+
+    Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (getKlass().getLeader() != null) {
+            return super.introduce().concat(" I am a Student. I am Leader of " + getKlass().getDisplayName() + ".");
+        } else {
+            return super.introduce().concat(" I am a Student. I am at " + getKlass().getDisplayName() + ".");
+        }
+    }
 }
